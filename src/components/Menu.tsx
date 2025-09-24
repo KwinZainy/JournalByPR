40768af975c7a876a0744510082bf7c84620257d
KwinZainy <gbadamosiblessing651@gmail.com>
2025-09-24 20:59:19 +0100

    menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -16,14 +16,14 @@ const Header = () => {
             </button>
             <div className=' '>
             {open &&(
-              <div className='bg-[#FAF8F2] w-full flex flex-col gap-12 items-end  h-1/3 py-10 fixed top-0 right-0  z-1000 overflow-y-hidden'>
+              <div className='bg-[#FAF8F2] w-full flex flex-col gap-12 items-end  h-1/3 py-8 fixed top-0 right-0  z-1000 overflow-y-hidden'>
                 <button className=' pr-10 ' onClick={() => setOpen(!open)} >
                   {open?  <X size={27} className='text-black'/> : <Menu size={25} className='text-black'/>}
                 </button>
-              <div className=' flex flex-col gap-4 mx-auto  max-tablet:mt-3 max-mobile:gap-2 it'>
-            <Link href={'/'} className=' text-[18px] font-medium'>Home</Link>
-            <Link href={'/aboutUs'} className=' text-[18px] font-medium'>About Us</Link>
-            <Link href={'/contactUs'} className=' text-[18px] font-medium'>Contact Us</Link>
+              <div className=' flex flex-col gap-6 mx-auto  max-tablet:mt-2 max-mobile:gap-2 it pb-10'>
+            <Link href={'/'} className=' text-[20px] font-medium shadow-md text-center rounded-[10px] px-3 py-2'>Home</Link>
+            <Link href={'/aboutUs'} className=' text-[20px] font-medium shadow-md text-center rounded-[10px] px-3 py-2'>About Us</Link>
+            <Link href={'/contactUs'} className=' text-[20px] font-medium shadow-md text-center rounded-[10px] px-3 py-2'>Contact Us</Link>
               </div>
               </div>
             )}

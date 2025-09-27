4e3961b7e85bf2bbc0696e940a244f51b416bb84
KwinZainy <gbadamosiblessing651@gmail.com>
2025-09-27 21:42:54 +0100

    adjustment

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -7,8 +7,8 @@ const ThankYouPage = () => {
     <PagesLayout>
       <main className="bg-[#FAF8F2] min-h-screen flex items-center justify-center">
         <div className="text-center max-w-lg">
-          <h1 className="text-4xl font-bold text-[#290303] mb-4">🎉 Thank You!</h1>
-          <p className="text-lg text-[#290303]">
+          <h1 className="text-4xl font-bold text-[#290303] mb-4"> Thank You!</h1>
+          <p className="text-[30px] text-[#290303]">
             Your message has been sent successfully. We’ll get back to you soon.
           </p>
         </div>

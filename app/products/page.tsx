6becb4252ca2f23d6af7b4b4f16c938bbf81b8c3
Kwin Zainy <gbadamosiblessing651@gmail.com>
2026-02-03 17:43:44 +0100

    fixed

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -10,6 +10,7 @@ export default function ProductsPage() {
     <section className="min-h-screen flex justify-center bg-[#FAF8F2]  px-6">
       <div className="text-center mt-30">
         <h1 className="text-4xl md:text-5xl font-bold text-[#290303] mb-4 max-mobile:mb-2">
+          {/* Our Products */}
           Our Products
         </h1>
         
